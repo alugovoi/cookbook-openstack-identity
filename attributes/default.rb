fa06474f47be26ea5e61e63921b22a2b9d977070
root <root@a1r1.ccpdev.cci.att.com>
2014-06-19 12:40:23 +0000

    Keystone v3 related commits that were missing

--- attributes/default.rb
+++ attributes/default.rb
@@ -73,6 +73,8 @@ default['openstack']['identity']['token']['backend'] = 'sql'
 default['openstack']['identity']['catalog']['backend'] = 'sql'
 default['openstack']['identity']['policy']['backend'] = 'sql'
 
+default['openstack']['identity']['api_v3_pipeline'] = 'access_log sizelimit url_normalize token_auth admin_token_auth xml_body json_body ec2_extension s3_extension endpoint_filter_extension service_v3'
+
 # LDAP backend general settings
 default['openstack']['identity']['ldap']['url'] = 'ldap://localhost'
 default['openstack']['identity']['ldap']['user'] = 'dc=Manager,dc=example,dc=com'
